ecb56a075c54e7008769a285eae5a7ec5ad58683
sadiqss <sadiq.sau48@gmail.com>
2022-03-14 21:18:41 +0000

    exploring destructuing option

--- js/countries.js
+++ js/countries.js
@@ -10,12 +10,33 @@ const displayCountries = countries => {
     const container = document.getElementById('countries');
     container.innerHTML = countriesHTML.join(' ');
 }
+/* const getCountryHTML = country => {
+    //option 1 destructuring
+    const { name, flags } = country;
 
-const getCountryHTML = country => {
+    return `
+    <div class='country'>
+    <h2>${name}</h2>
+    <img src="${flags.png}">
+    </div>
+    `
+} */
+//original
+/* const getCountryHTML = country => {
     return `
     <div class='country'>
     <h2>${country.name}</h2>
     <img src="${country.flags.png}">
+    </div>
+    `
+} */
+// option 2
+const getCountryHTML = ({ name, flags }) => {
+
+    return `
+    <div class='country'>
+    <h2>${name}</h2>
+    <img src="${flags.png}">
     </div>
     `
 }
